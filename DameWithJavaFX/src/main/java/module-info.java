344a3e37a18912cfbdafb61d4f42b549e0ad6aa2
Idris_d237 <96464111+geekers-donald237@users.noreply.github.com>
2022-11-01 14:56:20 +0000

    Add files via upload

---
+++ DameWithJavaFX/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module exoperso.damewithjavafx {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
+    opens exoperso.damewithjavafx to javafx.fxml;
+    exports exoperso.damewithjavafx;
+}
